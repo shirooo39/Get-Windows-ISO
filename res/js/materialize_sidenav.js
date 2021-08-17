7a777d7e3b1535e224a025344916a3bd5f522bb7
Shiro39 <38461122+shirooo39@users.noreply.github.com>
2021-08-17 22:01:53 +0700

    Added link for Windows 11 build 22000.132

--- res/js/materialize_sidenav.js
+++ res/js/materialize_sidenav.js
@@ -1,4 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() 
+{
 	var elems = document.querySelectorAll('.sidenav');
 	var instances = M.Sidenav.init(elems, {edge:'left'});
 });
